db5da1819bc44c7ba1e263d8c4f1fbb6b225aa51
Ed Hertzog <edhertzog@gmail.com>
2023-05-11 07:49:32 -0400

    Correct start-up

--- keyserver-terraform-start.sh
+++ keyserver-terraform-start.sh
@@ -4,4 +4,5 @@ sudo apt-get install -y docker.io
 gcloud auth print-access-token | docker login -u oauth2accesstoken --password-stdin us-east1-docker.pkg.dev
 docker run -dit -p 1234:1234 --name fennel-keyserver us-east1-docker.pkg.dev/whiteflag-0/fennel-docker-registry/keyserver:latest
 docker exec -it fennel-keyserver sh
-./opt/app/build-dev.sh
+cd /opt/app
+./build-dev.sh
