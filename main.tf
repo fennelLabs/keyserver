10c139e00f49365d81250703ff2c77d99e7b7bf7
Ed Hertzog <edhertzog@gmail.com>
2023-05-11 06:57:26 -0400

    Add startup

--- main.tf
+++ main.tf
@@ -39,8 +39,9 @@ resource "google_compute_instance" "fennel-keyserver" {
     sudo apt-get update
     sudo apt-get install -y docker.io
     gcloud auth print-access-token | docker login -u oauth2accesstoken --password-stdin us-east1-docker.pkg.dev
-    docker pull us-east1-docker.pkg.dev/whiteflag-0/fennel-docker-registry/fennel-keyserver:latest
     docker run -dit -p 1234:1234 --name fennel-keyserver us-east1-docker.pkg.dev/whiteflag-0/fennel-docker-registry/fennel-keyserver:latest
+    docker exec -it fennel-keyserver sh
+    ./opt/app/build-dev.sh
   EOF  
 
  metadata = {
