397dc9a906ce1815c5897b9f597e8759105a3bbd
Ed Hertzog <edhertzog@gmail.com>
2023-05-11 07:44:50 -0400

    Correct path to image

--- keyserver-terraform-start.sh
+++ keyserver-terraform-start.sh
@@ -2,6 +2,6 @@
 sudo apt-get update
 sudo apt-get install -y docker.io
 gcloud auth print-access-token | docker login -u oauth2accesstoken --password-stdin us-east1-docker.pkg.dev
-docker run -dit -p 1234:1234 --name fennel-keyserver us-east1-docker.pkg.dev/whiteflag-0/fennel-docker-registry/fennel-keyserver:latest
+docker run -dit -p 1234:1234 --name fennel-keyserver us-east1-docker.pkg.dev/whiteflag-0/fennel-docker-registry/keyserver:latest
 docker exec -it fennel-keyserver sh
 ./opt/app/build-dev.sh
